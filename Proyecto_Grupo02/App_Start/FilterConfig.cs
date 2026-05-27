3deb4d2b829340f023ce2f4df40fcc8c084e6f16
crysblancos <crysblancos120@gmail.com>
2026-05-27 16:54:56 -0600

    Version inicial Proyecto

---
+++ Proyecto_Grupo02/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Grupo02
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Proyecto_Grupo02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Grupo02.MvcApplication" Language="C#" %>
