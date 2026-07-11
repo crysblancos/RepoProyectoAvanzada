d2fff649e299722dcca64b43a4e5832ae473ca38
valentinr90 <rodriguezugarte90@gmail.com>
2026-07-10 21:51:01 -0600

    Productos y detalle Productos

--- Proyecto_Grupo02/Controllers/HomeController.cs
+++ Proyecto_Grupo02/Controllers/HomeController.cs
@@ -28,5 +28,14 @@ namespace Proyecto_Grupo02.Controllers
             return View();
         }
 
+        public ActionResult Producto()
+        {
+            return View();
+        }
+
+        public ActionResult DetalleProducto()
+        {
+            return View();
+        }
     }
 }
